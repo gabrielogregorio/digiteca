dc284b7a60efc9fca7de5fd1657b7d5e77034769
Fabiano Silva <51839602+SanDevelOps@users.noreply.github.com>
2020-07-04 16:30:53 -0300

    Reestruturação do escopo do projeto

---
+++ exemplo.php
@@ -0,0 +1,17 @@
+<?php include('layout/header.html');        ?>
+
+
+
+
+<h1>Olá, Sou um exemplo de como incluir o arquivo php</h1>
+
+
+
+
+
+
+
+
+
+
+<?php include('layout/footer.html');        ?>
---
+++ home.php
@@ -0,0 +1,20 @@
+<?php include('layout/header.html'); ?>
+
+
+
+Colocar todas as coisas necessarias aqui dentro
+
+
+
+<?php include('layout/footer.html'); ?>
+
+
+
+
+
+
+
+
+
+
+
