5e54cf683a8981db6e697e22a3837e9900308868
Gabriel Gregório da Silva <gabriel.gregorio.1@outlook.com>
2020-07-05 15:25:04 -0300

    Básico da tela de cadastro

--- cadUsuarios.php
+++ cadUsuarios.php
@@ -1,40 +1,49 @@
 <?php include('layout/header.html');?>
+<?php include('layout/navbar.php'); ?>
 
-<form action="cadUsuariosBD.php" method="post">
+<div class="page-container"  class="mx-auto">
+    <div class="content-wrap">
+        <div class="card card-header" id="card-style">
 
+			<form action="cadUsuariosBD.php" method="post">
 
-  <div class="form-group">
-    <label>CPF</label>
-    <input type="text" class="form-control" name="txtCPF" placeholder="Informe o CPF">
-  </div>
+			  <div class="form-group">
+			    <label>CPF</label>
+			    <input type="text" class="form-control" name="txtCPF" placeholder="Informe o CPF">
+			  </div>
 
-  <div class="form-group">
-    <label>Nome</label>
-    <input type="text" class="form-control" name="txtNOME" placeholder="Informe o nome">
-  </div>
+			  <div class="form-group">
+			    <label>Nome</label>
+			    <input type="text" class="form-control" name="txtNOME" placeholder="Informe o nome">
+			  </div>
 
-  <div class="form-group">
-    <label>Sobrenome</label>
-    <input type="text" class="form-control" name="txtSOBRENOME" placeholder="Informe o sobrenome">
-  </div>
+			  <div class="form-group">
+			    <label>Sobrenome</label>
+			    <input type="text" class="form-control" name="txtSOBRENOME" placeholder="Informe o sobrenome">
+			  </div>
 
-  <div class="form-group">
-    <label>Endereço de email</label>
-    <input type="email" class="form-control" name="txtEMAIL" placeholder="Informe o e-mail">
-  </div>
+			  <div class="form-group">
+			    <label>Endereço de email</label>
+			    <input type="email" class="form-control" name="txtEMAIL" placeholder="Informe o e-mail">
+			  </div>
 
-  <div class="form-group">
-    <label>Celular principal</label>
-    <input type="text" class="form-control" name="txtTELEFONE" placeholder="Informe o celular">
-  </div>
+			  <div class="form-group">
+			    <label>Celular principal</label>
+			    <input type="text" class="form-control" name="txtTELEFONE" placeholder="Informe o celular">
+			  </div>
 
-  <div class="form-group">
-    <label>Data de nascimento</label>
-    <input type="text" class="form-control" name="txtDATA_NASCIMENTO" placeholder="Informe a data de nascimento">
-  </div>
+			  <div class="form-group">
+			    <label>Data de nascimento</label>
+			    <input type="text" class="form-control" name="txtDATA_NASCIMENTO" placeholder="Informe a data de nascimento">
+			  </div>
 
-  <button type="submit" class="btn btn-primary">Cadastrar</button>
+			  
+			  <button type="button" class="btn btn-danger" onclick="history.go(-1)">Cancelar</button>
+			  <button type="submit" class="btn btn-success">Cadastrar</button>
 
-</form>
+			</form>
+		</div>
+	</div>
+</div>
 
 <?php include('layout/footer.html');?>
