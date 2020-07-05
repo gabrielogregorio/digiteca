8fc7acba816708581717558afeb0174f2394e428
Gabriel Gregório da Silva <gabriel.gregorio.1@outlook.com>
2020-07-05 17:24:27 -0300

    Cadastro de empréstimo telas

--- cadEmprestimos.php
+++ cadEmprestimos.php
@@ -1,7 +1,8 @@
 <?php include('layout/header.html');?>
 <?php include('layout/navbar.php'); ?>
+<?php include("recursos.php"); ?>
 
-<div class="page-container"  class="mx-auto">
+<div class="page-container"  class="mx-auto"> 
     <div class="content-wrap">
         <div class="card card-header" id="card-style">
 
@@ -27,35 +28,34 @@
     			        <option value="<?php echo $linha["ISBN"]; ?>" > <?php echo $linha["TITULO"]; ?> </option>
 					<?php }} ?>
 
-			      </select>
+			      </select name="txtLIVRO_ISBN">
 			    </div>
 
 
 			  <div class="form-group">
 			    <label>CPF</label>
-			    <input type="text" class="form-control" name="txtCPF" placeholder="Informe o CPF do usuário">
+			    <input type="text" class="form-control" name="txtCPF_PESSOA" placeholder="Informe o CPF do usuário" id="cpf_usuario" onblur="">
 			  </div>
 
-
-			  <div class="form-group">
+			  <!--<div class="form-group">
 			    <label>Nome</label>
-			  <input class="form-control" id="disabledInput" type="text" placeholder="Gabriel Gregório da Silva" disabled>
-			</div>
-
+			  <input class="form-control" id="nome_usuario" type="text" placeholder="" disabled>
+			</div>-->
 
 			  <div class="form-group">
-			    <label>Data do Empréstimo</label>
-			  <input class="form-control" id="disabledInput" type="text" placeholder="22/02/2020" disabled>
+			    <label >Data do Empréstimo</label>
+			  <input class="form-control" id="data_emprestimo" name= "txtDATA_EMPRESTADO" type="text" placeholder="<?php obter_data_dd_mm_yyyy() ?>" disabled> 
 			</div>
 
 			  <div class="form-group">
 			    <label>Dias que o livro permanecerá emprestado</label>
-			  <input class="form-control" id="disabledInput" type="text" placeholder="5" disabled>
+			  <input class="form-control" name="txtTEMPO_EMPRESTIMO" type="text" placeholder="Quantos dias o livro ficará emprestado">
 			</div>
 
 
-				  <button type="button" class="btn btn-danger" onclick="history.go(-1)">Cancelar</button>
-				  <button type="submit" class="btn btn-success">Emprestar</button>
+		  <button type="button" class="btn btn-danger" onclick="history.go(-1)">Cancelar</button>
+		  <button type="submit" class="btn btn-success" >Emprestar</button>
+		  <!-- type="submit-->
 
 			</form>
 		</div>
