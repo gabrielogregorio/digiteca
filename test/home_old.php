a735487552c8d3a22fed20207431d7c99bcad459
Fabiano Silva <51839602+SanDevelOps@users.noreply.github.com>
2020-07-07 10:12:57 -0300

    backup arquivos home e index

---
+++ test/home_old.php
@@ -0,0 +1,18 @@
+<?php 
+    include('layout/header.html');
+    include('layout/navbar.php');
+
+    include('seguranca/seguranca.php');
+    
+    session_start();
+
+    if(administrador_logado() == false) {
+        header("location: index.php");
+        exit;
+    }
+ ?>
+    <div class="home">
+        
+    </div>
+
+<?php include('layout/footer.html'); ?>
